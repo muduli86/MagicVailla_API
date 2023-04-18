42d7aab72bfbec4b307e9d489ea5f39002af50b5
Muduli, Simanta <Simanta.Muduli@cdk.com>
2023-04-18 10:47:22 +0530

    First Endpoint

---
+++ MagicVailla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,26 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using MagicVilla_VillaAPI.Data;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVillaById(int id)
+        {
+            return VillaStore.villaList?.FirstOrDefault(x=>x.Id==id);
+        }
+    }
+}
---
+++ MagicVailla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO { Id = 1, Name = "Mariot0" },
+            new VillaDTO { Id = 2, Name = "Mariot1" },
+            new VillaDTO { Id = 3, Name = "Mariot2" },
+            new VillaDTO { Id = 4, Name = "Mariot3" },
+        };
+    }
+}
